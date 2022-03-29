e7005f8830a090740c5041b6579de55e2f4d917a
Mrulay <52288782+Mrulay@users.noreply.github.com>
2022-03-29 02:07:22 -0400

    Update entity_sentiment_analysis.py

--- entity_sentiment_analysis.py
+++ entity_sentiment_analysis.py
@@ -5,9 +5,8 @@ import numpy as np
 from Levenshtein import distance as lsd
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/content/NLP_W2022/Code/gcp_creds.json"
 client = language_v1.LanguageServiceClient()
-type_ = language_v1.types.Document.Type.PLAIN_TEXT
 language = "en"
-encoding_type = language_v1.EncodingType.UTF8
+encoding_type = "UTF8"
 
 with open(r'/content/NLP_W2022/Entities/StereosetEntities.pkl','rb') as file_handle:
     ss_ents = pickle.load(file_handle)
@@ -16,15 +15,15 @@ def filter(entities):
     return_ents = []
     for i, entity in enumerate(entities):
         distances = [lsd(entity,word) for word in ss_ents]
-        if min(distances) < 4: 
+        if min(distances) < 2: 
             return_ents.append(entity)
     return return_ents 
 
 def get_entity_sentiment(text):
     ents = []
     sents = []
-    document = {"content": text, "type_": type_, "language": language}
-    response = client.analyze_entity_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    document = {"content": text, "type": "PLAIN_TEXT", "language": language}
+    response = client.analyze_entity_sentiment(document,encoding_type)
     for entity in response.entities:
             sentiment = entity.sentiment
             ents.append(entity.name)
